e1fa484a5266adf1a4e867928b69166752018f10
shadil-rayyan <shadilrayyan2@gmail.com>
2025-12-10 00:32:35 +0530

    fix (ascii,boot,install) we changed the git clone repo changed the ascii art changed the omakub text name to codekub

--- boot.sh
+++ boot.sh
@@ -2,24 +2,25 @@
 
 set -e
 
-ascii_art='________                  __        ___.
-\_____  \   _____ _____  |  | ____ _\_ |__
- /   |   \ /     \\__   \ |  |/ /  |  \ __ \
-/    |    \  Y Y  \/ __ \|    <|  |  / \_\ \
-\_______  /__|_|  (____  /__|_ \____/|___  /
-        \/      \/     \/     \/         \/
+ascii_art='   _____          _      _  __     _     
+  / ____|        | |    | |/ /    | |    
+ | |     ___   __| | ___| \' / ___ | |__  
+ | |    / _ \ / _` |/ _ \  < / _ \| \'_ \ 
+ | |___| (_) | (_| |  __/ . \ (_) | |_) |
+  \_____\___/ \__,_|\___|_|\_\___/|_.__/ 
 '
 
+
 echo -e "$ascii_art"
-echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
+echo "=> codekub is for fresh Ubuntu 24.04+ installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
-echo "Cloning Omakub..."
+echo "Cloning codekub..."
 rm -rf ~/.local/share/omakub
-git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+git clone https://github.com/CodeCompasss/codekub.git ~/.local/share/omakub >/dev/null
 if [[ $OMAKUB_REF != "master" ]]; then
 	cd ~/.local/share/omakub
 	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
